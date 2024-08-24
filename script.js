db961fe354ff8cab5b7741334d3a10c10227a8fe
fidelisobioma <mbamfidelisobioma1@gmail.com>
2024-08-24 03:10:39 +0100

    creates single box

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const boxRows = 10;
 const boxCols = 10;
 let boxes = boxRows * boxCols;
 let boxSize = cWidth / boxRows;
+//create single box
+let createBoxes = () => {
+  const box = document.createElement("div");
+  box.style.cssText = `box-sizing: border-box;width: ${boxSize}px;height: ${boxSize}px; background-color:#ccc;  border: 1px solid white;`;
+  container.appendChild(box);
+};
+createBoxes();
