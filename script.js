46857f716c6727825bebaa98d2bf11363a028a6a
fidelisobioma <mbamfidelisobioma1@gmail.com>
2024-08-24 02:58:03 +0100

    Added box size

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const container = document.querySelector(".grid_container");
 const cWidth = 300;
 const cHeight = 300;
 container.style.cssText = `width:${cWidth}px; height: ${cHeight}px;`;
+//Adding box size
+const boxRows = 10;
+const boxCols = 10;
+let boxes = boxRows * boxCols;
+let boxSize = cWidth / boxRows;
