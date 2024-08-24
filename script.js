689b694c88587c7c2dc36235c7c7afc809e88e1c
fidelisobioma <mbamfidelisobioma1@gmail.com>
2024-08-24 02:54:07 +0100

    Updated script file
    Assigned width and height to the div component

---
+++ script.js
@@ -0,0 +1,5 @@
+//Dynamicallly styling div element
+const container = document.querySelector(".grid_container");
+const cWidth = 300;
+const cHeight = 300;
+container.style.cssText = `width:${cWidth}px; height: ${cHeight}px;`;
